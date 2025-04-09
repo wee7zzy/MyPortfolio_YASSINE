27a54f119e766c0148fa68d55c3fafe18a34c576
wee7zzy <yassaniq@gmail.com>
2025-04-09 17:59:23 +0100

    ajout du component header et nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "../nav/nav.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
